77343357326bc9fd8dd349076fc61ce6a47dee51
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-03-12 14:53:18 +0300

    12.03.2022

--- static/js/cart.js
+++ static/js/cart.js
@@ -18,4 +18,21 @@ for (i = 0; i < updateBtns.length; i++){
 
 function updateUserOrder(productId, action){
   console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+    console.log('URL:', url)
+    fetch(url, {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+        'X-CSRFToken':csrftoken,
+      },
+      body: JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      location.reload();
+    });
 }
