988394c84bccb78c661217de4b36dc5e6dca0298
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-03-13 19:31:26 +0300

    13.03.2022

--- store/views.py
+++ store/views.py
@@ -39,6 +39,9 @@ def cart(request):
     return render (request, 'store/cart.html', context)
 
 
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -79,6 +82,9 @@ def updateItem(request):
     return JsonResponse('Item was added', safe = False)
 
 
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
@@ -87,12 +93,21 @@ def processOrder(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False )
         total = data['form']['total']
-        oder.transaction_id = transaction_id
+        order.transaction_id = transaction_id
 
-        if total == order.get_cart_total:
+        if (int(total) == int(order.get_cart_total)):
             order.complete = True
         order.save()
 
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode']
+            )
 
     else:
         print('User is not logged in..')
