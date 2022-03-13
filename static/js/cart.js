52a4f5fbf53e4eb5da722206751c56818b8b6363
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-03-13 23:39:13 +0300

    13.03.2022

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,15 +4,41 @@ for (i = 0; i < updateBtns.length; i++){
   updateBtns[i].addEventListener('click', function(){
     var productId = this.dataset.product
     var action = this.dataset.action
+    console.log(action)
     console.log('productId:', productId, 'Action:', action)
 
     console.log('USER:', user)
-    if (user == 'AnonymousUser'){
-      console.log('User is not authenticated')
+    if (user === 'AnonymousUser'){
+      addCookieItem(productId, action)
     }else{
       updateUserOrder(productId, action)
     }
   })
+}
+
+
+function addCookieItem(productId, action){
+  console.log('Not logged in..')
+  console.log(action)
+  if (action == 'add'){
+    if (cart[productId] == undefined){
+      cart[productId] = {'quantity':1}
+    }else{
+      cart[productId]['quantity'] += 1
+    }
+  }
+
+  if (action == 'remove'){
+    cart[productId]['quantity'] -=1
+
+    if (cart[productId]['quantity'] <=0){
+      console.log('Item should be deleted')
+      delete cart[productId]
+    }
+  }
+  console.log('Cart:', cart)
+  document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+  location.reload()
 }
 
 
